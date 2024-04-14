9856a4bf2ea82275f3ca43c82c9110f72a7dc90e
dlord213 <lollingworld41@gmail.com>
2024-04-14 15:55:30 +0800

    Update

--- App.js
+++
@@ -1,8 +0,0 @@
-import { Pressable, Text, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-
-export default function App() {
-  return (
-    <View></View>
-  );
-}
